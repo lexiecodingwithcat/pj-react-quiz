98ed8158106bb113bf18cbe135dca325885747d6
Tianzi Cui <cuitianzi0@gmail.com>
2024-07-03 16:09:31 -0600

    set up react quiz app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
-
-
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
-      
+
+      <Main>
+        <p>1/15</p>
+        <p>question?</p>
+      </Main>
     </div>
   );
 }
