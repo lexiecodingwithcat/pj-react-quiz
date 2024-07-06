c1afef423f1dad09e855e0b40322f90f02d50f09
Tianzi Cui <cuitianzi0@gmail.com>
2024-07-05 18:34:35 -0600

    handle new answers

---
+++ src/components/Options.js
@@ -0,0 +1,17 @@
+function Options({ question, dispatch, answer }) {
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className="btn btn-option"
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
