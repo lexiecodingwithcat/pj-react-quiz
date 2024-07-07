26915b727a330eb96ba22f603bd559f0d53e0834
Tianzi Cui <cuitianzi0@gmail.com>
2024-07-07 01:05:34 -0600

    implement emoji ahead of scores

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,9 +1,15 @@
 function FinishScreen({ points, maxPossiblePoints }) {
-  const precentage = (points / maxPossiblePoints) * 100;
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🍾";
+  if (percentage >= 50 && percentage < 80) emoji = "🤨";
+  if (percentage >= 0 && percentage < 50) emoji = "😶";
+  if (percentage === 0) emoji = "🤦‍♀️";
   return (
     <p className="result">
-      You scored <strong>{points}</strong> out of {maxPossiblePoints}(
-      {Math.ceil(precentage)}%)
+      {emoji}You scored <strong>{points}</strong> out of {maxPossiblePoints}(
+      {Math.ceil(percentage)}%)
     </p>
   );
 }
